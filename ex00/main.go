490d4aaf0cfbc9d9fa8a43a013050c25e72fe9ca
tyamagis <tyamagis@outlook.com>
2022-04-30 13:57:59 +0900

    add printstr()

--- ex00/main.go
+++ ex00/main.go
@@ -1,10 +1,31 @@
 package main
 
-import p "piscine"
+import (
+	p "piscine"
+	"ft"
+)
 
 func checkmate(board []string){
-
+	if CheckBoard(board) == false {
+		// print error
+	}
+	if GetPosition(board) == false {
+		// print error
+	}
+	if Judge(board) {
+		ft.PrintStr("Success\n")
+	} else {
+		ft.PrintStr("Fail\n")
+	}
 }
 
 func main(){
+	board := []string{
+		"R...",
+		".K..",
+		"..P.",
+		"....",
+	}
+
+	checkmate(board);
 }
